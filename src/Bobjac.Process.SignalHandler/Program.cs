3fab566b4abcdea67bc915e5836c736e538f9ca2
Bob Jacobs <bob.jacobs@outlook.com>
2020-06-26 10:42:51 -0400

    initial commit

---
+++ src/Bobjac.Process.SignalHandler/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Bobjac.Process.SignalHandler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
